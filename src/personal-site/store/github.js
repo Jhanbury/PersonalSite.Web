0337a4be45416594c84cb9d4760034f7e96f42a5
John Hanbury <johnhanbury4@gmail.com>
2020-09-22 10:32:20 +0100

    updated packages, updated api calls updated social links

--- src/personal-site/store/github.js
+++ src/personal-site/store/github.js
@@ -9,7 +9,7 @@ export const mutations = {
 }
 export const actions = {
   async getGithubRepos({ commit }, userId) {
-    const fullUrl = `api/userinfo/${userId}/coderepos`
+    const fullUrl = `/api/user/${userId}/repos`
     const result = await this.$axios.get(fullUrl)
     if (result) {
       commit('update', result.data)
--- src/personal-site/store/projects.js
+++ src/personal-site/store/projects.js
@@ -9,7 +9,7 @@ export const mutations = {
 }
 export const actions = {
   async getProjects({ commit }, userId) {
-    const fullUrl = `api/userinfo/${userId}/projects`
+    const fullUrl = `/api/user/${userId}/projects`
     const result = await this.$axios.get(fullUrl)
     if (result) {
       commit('update', result.data)
